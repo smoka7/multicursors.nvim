c9c750d3680984fee613e219712a6833c82a91d9
mohsen <smoka7@hotmail.com>
2023-06-19 18:22:54 +0330

    feat: added find next match and skip current

--- lua/multicursors/init.lua
+++ lua/multicursors/init.lua
@@ -52,6 +52,39 @@ local create_extmark = function(match)
     })
 end
 
+
+--- Returns the first match for pattern after a offset in a string
+---@param string string
+---@param pattern string
+---@param row_idx integer
+---@param offset integer
+---@return integer? id of created mark
+local find_next_match = function(string, pattern, row_idx, offset)
+    if not string or string == '' then
+        return
+    end
+
+    if offset ~= 0 then
+        string = string:sub(offset + 1, -1)
+    end
+
+    local match = vim.fn.matchstrpos(string, '\\<' .. pattern .. '\\>')
+    -- -1 range means not found
+    if match[2] == -1 and match[3] == -1 then
+        return nil
+    end
+
+    -- add offset to match position index
+    match.start = match[2] + offset
+    match.finish = match[3] + offset
+
+    -- jump the cursor to last char of match
+    match.row = row_idx
+    move_cursor({ row_idx, match.finish }, nil)
+
+    return create_extmark(match)
+end
+
 -- creates a mark for word under the cursor
 ---@return integer?,string?
 M.find_cursor_word = function()
@@ -78,4 +111,51 @@ M.find_cursor_word = function()
     move_cursor { cursor[1], cursor[2] + right[3] }
 
     return mark_id, left[1] .. right[1]:sub(2)
+end
+
+--- skips current match and jumps to next
+---@param pattern string
+---@param current_mark? integer
+M.skip_forward = function(pattern, current_mark)
+    if not current_mark then
+        return
+    end
+    api.nvim_buf_del_extmark(0, ns_id, current_mark)
+    M.find_next(pattern)
+end
+
+---finds next match and marks it
+---@param pattern string
+---@return integer? id of next match mark
+M.find_next = function(pattern)
+    ---@type integer[]
+    local cursor = api.nvim_win_get_cursor(0)
+    local line_count = api.nvim_buf_line_count(0)
+
+    local row_idx = cursor[1]
+    -- search the same line as cursor with cursor col as offset cursor
+    local line = api.nvim_buf_get_lines(0, row_idx - 1, row_idx, true)[1]
+    local mark_id = find_next_match(line, pattern, row_idx, cursor[2])
+    if mark_id then
+        return mark_id
+    end
+
+    -- search from cursor to end of buffer for pattern
+    for idx = row_idx + 1, line_count, 1 do
+        line = api.nvim_buf_get_lines(0, idx - 1, idx, true)[1]
+        mark_id = find_next_match(line, pattern, idx, 0)
+        if mark_id then
+            return mark_id
+        end
+    end
+
+    -- when we didn't find the pattern we start searching again
+    -- from start of the buffer
+    for idx = 0, cursor[1], 1 do
+        line = api.nvim_buf_get_lines(0, idx - 1, idx, true)[1]
+        mark_id = find_next_match(line, pattern, idx, 0)
+        if mark_id then
+            return mark_id
+        end
+    end
 end
