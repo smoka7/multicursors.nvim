fd703b10a0385d192a4ab8ed85dbf4f47e2fb91d
mohsen <smoka7@hotmail.com>
2023-06-25 10:25:37 +0330

    refactor:create a normal mode module

--- lua/multicursors/utils.lua
+++ lua/multicursors/utils.lua
@@ -5,6 +5,32 @@ local ns_id = api.nvim_create_namespace 'multicursors'
 
 local M = {}
 
+--- creates a extmark for the the match
+--- doesn't create a duplicate mark
+---@param match Match
+---@return integer id of created mark
+M.create_extmark = function(match)
+    local marks = api.nvim_buf_get_extmarks(
+        0,
+        ns_id,
+        { match.row - 1, match.start },
+        { match.row - 1, match.finish },
+        {}
+    )
+    if #marks > 0 then
+        debug('found ' .. #marks .. ' duplicate marks:')
+        return marks[1][1]
+    end
+
+    local s = api.nvim_buf_set_extmark(0, ns_id, match.row - 1, match.start, {
+        end_row = match.row - 1,
+        end_col = match.finish,
+        hl_group = 'MultiCursor',
+    })
+    vim.cmd [[ redraw! ]]
+    return s
+end
+
 ---@param any any
 M.debug = function(any)
     if config.DEBUG_MODE then
@@ -26,6 +52,18 @@ M.getAllCursors = function(details)
         { details = details }
     )
     return extmarks
+end
+
+--- gets a single char from user
+--- when intrupted returns nil
+---@return string?
+M.get_char = function()
+    local ok, key = pcall(vim.fn.getcharstr)
+    if not ok then
+        return nil
+    end
+
+    return key
 end
 
 ---
@@ -65,6 +103,24 @@ M.insert_text = function(text, skip_current)
             )
         end
     end
+end
+
+--- Moves the cursor to pos and marks current cursor position in jumplist
+--- htpps://github.com/neovim/neovim/issues/20793
+---@param pos any[]
+---@param current any[]?
+M.move_cursor = function(pos, current)
+    if not current then
+        current = api.nvim_win_get_cursor(0)
+    end
+
+    api.nvim_buf_set_mark(0, "'", current[1], current[2], {})
+    api.nvim_win_set_cursor(0, { pos[1], pos[2] })
+    vim.cmd [[ redraw! ]]
+end
+
+M.exit = function()
+    api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
 end
 
 return M
