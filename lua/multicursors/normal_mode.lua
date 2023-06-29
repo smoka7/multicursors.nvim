f61dfe2f0d40690559beb09d41429bc539479bbd
mohsen <smoka7@hotmail.com>
2023-06-29 16:41:50 +0330

    feat:added find prev and skip prev

--- lua/multicursors/normal_mode.lua
+++ lua/multicursors/normal_mode.lua
@@ -56,6 +56,56 @@ local find_next_match = function(string, last_match, row_idx, offset, skip)
 
     return found
 end
+
+--- Returns the last match before the cursor
+---@param string string
+---@param last_match Match
+---@param row_idx integer
+---@param till integer
+---@param skip boolean
+---@return Match?
+local find_prev_match = function(string, last_match, row_idx, till, skip)
+    if not string or string == '' then
+        return
+    end
+    local sub = string
+    if till ~= -1 then
+        sub = string:sub(0, till)
+    end
+    ---@type any[]?
+    local match = nil
+    ---@type Match?
+    local found = nil
+    local offset = 0
+    repeat
+        match = vim.fn.matchstrpos(sub, '\\<' .. last_match.pattern .. '\\>')
+        -- -1 range means not found
+        if match[2] ~= -1 and match[3] ~= -1 then
+            found = {
+                pattern = last_match.pattern,
+                start = match[2] + offset, -- add offset to match position index
+                finish = match[3] + offset,
+                row = row_idx,
+            }
+            offset = offset + match[3]
+            sub = string:sub(offset + 1, till)
+        end
+    until match and match[2] == -1 and match[3] == -1
+
+    if not found then
+        return
+    end
+
+    -- jump the cursor to last match
+    api.nvim_buf_clear_namespace(0, main_cursor, 0, -1)
+    if not skip then
+        utils.create_extmark(last_match, ns_id)
+    end
+    utils.create_extmark(found, main_cursor)
+    utils.move_cursor({ row_idx + 1, found.start }, nil)
+
+    return found
+end
 --
 -- creates a mark for word under the cursor
 ---@return Match?
@@ -121,6 +171,52 @@ M.find_next = function(last_match, skip)
     end
 end
 
+---finds previous match and marks it
+---@param last_match Match?
+---@param skip boolean
+---@return Match? prev previus match
+M.find_prev = function(last_match, skip)
+    if not last_match then
+        return
+    end
+    local line_count = api.nvim_buf_line_count(0)
+    local row_idx = last_match.row
+    local column = last_match.finish
+
+    -- search the same line untill the cursor
+    local line = api.nvim_buf_get_lines(0, row_idx, row_idx + 1, true)[1]
+    local match = find_prev_match(
+        line,
+        last_match,
+        row_idx,
+        column - #last_match.pattern,
+        skip
+    )
+    if match then
+        return match
+    end
+
+    -- search from cursor to beginning of buffer for pattern
+    -- fo
+    for idx = row_idx - 1, 0, -1 do
+        line = api.nvim_buf_get_lines(0, idx, idx + 1, true)[1]
+        match = find_prev_match(line, last_match, idx, -1, skip)
+        if match then
+            return match
+        end
+    end
+
+    -- when we didn't find the pattern we start searching again
+    -- from start of the buffer
+    for idx = line_count - 1, row_idx, -1 do
+        line = api.nvim_buf_get_lines(0, idx, idx + 1, true)[1]
+        match = find_prev_match(line, last_match, idx, -1, skip)
+        if match then
+            return match
+        end
+    end
+end
+
 M.start = function()
     local last_mark = M.find_cursor_word()
 
@@ -146,8 +242,12 @@ M.listen = function(last_mark)
             return
         elseif key == 'n' then
             last_mark = M.find_next(last_mark, false)
+        elseif key == 'N' then
+            last_mark = M.find_prev(last_mark, false)
         elseif key == 'q' then
             last_mark = M.find_next(last_mark, true)
+        elseif key == 'Q' then
+            last_mark = M.find_prev(last_mark, true)
         elseif key == 'i' then
             vim.cmd.startinsert()
             insert_mode.start()
