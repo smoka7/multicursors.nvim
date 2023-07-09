c5d6d9c9119b98ae91ddecdaecb1a9136f3352cf
mohsen <smoka7@hotmail.com>
2023-07-09 16:30:29 +0330

    feat!: use layered mappings

--- lua/multicursors/insert_mode.lua
+++ lua/multicursors/insert_mode.lua
@@ -5,14 +5,12 @@ local api = vim.api
 ---@class InsertMode
 ---@field _au_group number
 ---@field _inserted_text string
----@field _user_mappings any[]?
 local M = {}
 
 M._inserted_text = ''
 
 M._au_group = api.nvim_create_augroup('multicursors', { clear = true })
 
-M._user_mappings = nil
 M._user_update_time = 4000
 
 ---
@@ -23,8 +21,6 @@ M._on_insert_enter = function(config)
         callback = function()
             M._user_update_time = vim.opt.updatetime
             vim.opt.updatetime = config.updatetime
-            M._save_user_mappings()
-            M._register_mappings()
         end,
     })
 end
@@ -56,40 +52,23 @@ M._on_insert_leave = function()
         group = M._au_group,
         callback = function()
             vim.opt.updatetime = M._user_update_time
-            M._restore_user_mappings()
             if M._inserted_text == '' then
-                M.exit()
                 return
             end
             utils.insert_text(M._inserted_text)
             M._inserted_text = ''
-            M.exit()
         end,
     })
 end
 
 ---@param config Config
 local function set_insert_autocommands(config)
+    vim.b.MultiCursorInsert = true
     M._on_insert_enter(config)
     M._on_cursor_hold()
     M._on_insert_char_pre()
     M._on_insert_leave()
     vim.cmd.startinsert()
-end
-
-M._save_user_mappings = function()
-    M._user_mappings = api.nvim_get_keymap 'i'
-end
-
-M._register_mappings = function()
-    for _, map in ipairs(M._insert_mode_mappings) do
-        vim.keymap.set(
-            'i',
-            map.lhs,
-            map.rhs,
-            { expr = false, buffer = true, noremap = true }
-        )
-    end
 end
 
 M._insert_and_clear = function()
@@ -98,28 +77,6 @@ M._insert_and_clear = function()
     end
     utils.insert_text(M._inserted_text)
     M._inserted_text = ''
-end
-
---- returns true if we have a mapping for lhs in insert mode
----@param lhs string
----@return boolean
-local mapped = function(lhs)
-    for _, map in ipairs(M._user_mappings) do
-        if lhs == map.lhs then
-            vim.keymap.set('i', map.lhs, map.rhs or map.callback)
-            return true
-        end
-    end
-    return false
-end
-
-M._restore_user_mappings = function()
-    for _, map in ipairs(M._insert_mode_mappings) do
-        if not mapped(map.lhs) then
-            vim.keymap.del('i', map.lhs, { buffer = 0 })
-        end
-    end
-    M._user_mappings = nil
 end
 
 M.BS_method = function()
@@ -152,15 +109,6 @@ M.Down_method = function()
     utils.move_selections_vertical(1)
 end
 
----@type Mapping[]
-M._insert_mode_mappings = {
-    { lhs = '<BS>', rhs = M.BS_method },
-    { lhs = '<Left>', rhs = M.Left_method },
-    { lhs = '<Right>', rhs = M.Right_method },
-    { lhs = '<Up>', rhs = M.UP_method },
-    { lhs = '<Down>', rhs = M.Down_method },
-}
-
 --- listens for every char press and inserts the text before leaving insert mode
 --TODO esc go to multicursor normal
 ---@param config Config
@@ -177,7 +125,12 @@ end
 
 M.exit = function()
     api.nvim_clear_autocmds { group = M._au_group }
-    utils.exit()
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes('<Esc>', true, false, true),
+        'n',
+        true
+    )
+    vim.b.MultiCursorInsert = nil
     vim.cmd [[redraw!]]
 end
 
