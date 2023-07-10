5b539661c6735a771bd79f4c90ab303cddc70d68
mohsen <smoka7@hotmail.com>
2023-07-10 18:12:04 +0330

    feat:add more actions in insert mode

--- lua/multicursors/insert_mode.lua
+++ lua/multicursors/insert_mode.lua
@@ -109,6 +109,78 @@ M.Down_method = function()
     utils.move_selections_vertical(1)
 end
 
+--- Inserts a new line at selections
+M.CR_method = function()
+    M._insert_and_clear()
+
+    local CR = api.nvim_replace_termcodes('<cr>', true, false, true)
+
+    utils.call_on_selections(function(mark)
+        api.nvim_win_set_cursor(0, { mark[1] + 1, mark[2] + 1 })
+        vim.cmd('normal! R' .. CR)
+    end, true, true)
+
+    utils.move_selections_vertical(1)
+    --HACK selections should start at line start
+    -- but move_selections_vertical doesn't changes col values
+    utils.move_selections_horizontal(-vim.v.maxcol)
+end
+
+--- Deletes the word backward
+M.C_w_method = function()
+    M._insert_and_clear()
+
+    local c_w = api.nvim_replace_termcodes('<C-w>', true, false, true)
+
+    utils.call_on_selections(function(mark)
+        api.nvim_win_set_cursor(0, { mark[1] + 1, mark[2] + 1 })
+        vim.cmd('normal! i' .. c_w)
+    end, true, true)
+
+    -- this does not change extmark position but when
+    -- we delete a text extmark start and end col will overlap and
+    -- user can't see the extmark when moving we increment end col so selection gets visible
+    utils.move_selections_horizontal(0)
+end
+
+--- Deletes the char under selection
+M.Del_method = function()
+    M._insert_and_clear()
+    utils.call_on_selections(function(mark)
+        api.nvim_win_set_cursor(0, { mark[1] + 1, mark[2] + 1 })
+        vim.cmd 'normal! x'
+    end, true, true)
+
+    utils.move_selections_horizontal(0)
+end
+
+--- Moves the selections to start of their lines
+M.Home_method = function()
+    M._insert_and_clear()
+
+    utils.move_selections_horizontal(-vim.v.maxcol)
+end
+
+--- Moves the selections to end of their lines
+M.End_method = function()
+    M._insert_and_clear()
+
+    utils.move_selections_horizontal(vim.v.maxcol)
+end
+
+--- Deletes selections til start of line
+M.C_u_method = function()
+    M._insert_and_clear()
+
+    local c_u = api.nvim_replace_termcodes('<C-u>', true, false, true)
+    utils.call_on_selections(function(mark)
+        api.nvim_win_set_cursor(0, { mark[1] + 1, mark[2] + 1 })
+        vim.cmd('normal! i' .. c_u)
+    end, true, true)
+
+    utils.move_selections_horizontal(0)
+end
+
 --- listens for every char press and inserts the text before leaving insert mode
 --TODO esc go to multicursor normal
 ---@param config Config
