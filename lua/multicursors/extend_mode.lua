e80a44e9fe91139c29f6d5d7df45ac4f158ba840
mohsen <smoka7@hotmail.com>
2023-07-16 22:37:02 +0330

    refactor: update extend types

--- lua/multicursors/extend_mode.lua
+++ lua/multicursors/extend_mode.lua
@@ -23,19 +23,19 @@ local function smaller(a, b)
 end
 
 --- Perform the motion and finds the new range for selection.
----@param mark any[]
+---@param mark Selection
 ---@param motion string
 ---@return Match
 local get_new_position = function(mark, motion)
     local new_pos
 
     -- modify float so it has same indexing as win_set_cursor
-    local float = { mark[1] + 1, mark[2] - 1 }
-    local anchor = { mark[3].end_row + 1, mark[3].end_col - 1 }
+    local float = { mark.row + 1, mark.col - 1 }
+    local anchor = { mark.end_row + 1, mark.end_col - 1 }
 
     if vim.b.MultiCursorAnchorStart then
-        anchor = { mark[1] + 1, mark[2] - 1 }
-        float = { mark[3].end_row + 1, mark[3].end_col - 1 }
+        anchor = { mark.row + 1, mark.col - 1 }
+        float = { mark.end_row + 1, mark.end_col - 1 }
     end
 
     -- goes to other end of selection based on anchor
@@ -73,21 +73,24 @@ end
 ---@type Match[]
 local last_selections = {}
 
+--- Saves current selections position
+---@param marks Selection[]
+---@param main Selection
 local function save_history(marks, main)
     last_selections = {}
-    for _, value in pairs(marks) do
+    for _, mark in pairs(marks) do
         table.insert(last_selections, {
-            s_row = value[2],
-            s_col = value[3],
-            e_row = value[4].end_row,
-            e_col = value[4].end_col,
+            s_row = mark.row,
+            s_col = mark.col,
+            e_row = mark.end_row,
+            e_col = mark.end_col,
         })
     end
     table.insert(last_selections, {
-        s_row = main[2],
-        s_col = main[3],
-        e_row = main[4].end_row,
-        e_col = main[4].end_col,
+        s_row = main.row,
+        s_col = main.col,
+        e_row = main.end_row,
+        e_col = main.end_col,
     })
 
     utils.clear_selections()
@@ -120,15 +123,12 @@ local extend_selections = function(motion)
 
     local new_pos
     for _, selection in pairs(marks) do
-        new_pos = get_new_position(
-            { selection[2], selection[3], selection[4] },
-            motion
-        )
+        new_pos = get_new_position(selection, motion)
 
         utils.create_extmark(new_pos, utils.namespace.Multi)
     end
 
-    new_pos = get_new_position({ main[2], main[3], main[4] }, motion)
+    new_pos = get_new_position(main, motion)
     utils.create_extmark(new_pos, utils.namespace.Main)
 end
 
@@ -186,10 +186,11 @@ E.o_method = function()
         utils.move_cursor { main[2] + 1, main[3] }
         return
     end
-    utils.move_cursor { main[4].end_row + 1, main[4].end_col - 1 }
+    utils.move_cursor { main.end_row + 1, main.end_col - 1 }
     vim.b.MultiCursorAnchorStart = true
 end
 
+---@param callback fun(mark:Match):Match
 local extend_selections_ts = function(callback)
     if not parsers.has_parser() then
         return
@@ -202,19 +203,19 @@ local extend_selections_ts = function(callback)
     local new_pos
     for _, selection in pairs(marks) do
         new_pos = callback {
-            s_row = selection[2],
-            s_col = selection[3],
-            e_row = selection[4].end_row,
-            e_col = selection[4].end_col,
+            s_row = selection.row,
+            s_col = selection.col,
+            e_row = selection.end_row,
+            e_col = selection.end_col,
         }
         utils.create_extmark(new_pos, utils.namespace.Multi)
     end
 
     new_pos = callback {
-        s_row = main[2],
-        s_col = main[3],
-        e_row = main[4].end_row,
-        e_col = main[4].end_col,
+        s_row = main.row,
+        s_col = main.col,
+        e_row = main.end_row,
+        e_col = main.end_col,
     }
     utils.create_extmark(new_pos, utils.namespace.Main)
 end
@@ -230,4 +231,5 @@ end
 E.node_last_child = function()
     extend_selections_ts(ts.get_last_child)
 end
+
 return E
