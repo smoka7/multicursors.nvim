7fa82b0cf952bac1007a6be89ff0ba9aff6b9d01
Maria José Solano <majosolano99@gmail.com>
2023-10-14 18:55:01 -0700

    fix: HeadOps doesn't have a description field

--- lua/multicursors/layers.lua
+++ lua/multicursors/layers.lua
@@ -30,9 +30,9 @@ local generate_heads = function(keys, nowait, show_desc)
         if action.method ~= false then
             local opts = action.opts or {}
 
-            opts.description = nil
+            opts.desc = nil
             if action.opts and show_desc then
-                opts.description = action.opts.desc
+                opts.desc = action.opts.desc
             end
 
             if action.opts.nowait ~= nil then
